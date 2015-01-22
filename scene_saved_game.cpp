af78f6033996b4b522835b1fb55e3b4f63a2b8eb
Rohit Nirmal <rohitnirmal9@gmail.com>
2015-01-22 14:54:52 -0600

    Remove unused #include.

--- scene_saved_game.cpp
+++ scene_saved_game.cpp
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with Clonepoint.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <fstream>
 #include "scene.h"
 
 #define SAVE_INTERVAL_MS 3000
